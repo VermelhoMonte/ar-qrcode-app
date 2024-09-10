654af599acfdb0dd5948fa11513b0792f0cc560b
Vermelho Monte <vermelho.monte@gmail.com>
2024-09-10 20:11:59 -0300

    correcao video0810

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.150.0';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.150.0/examples/jsm/loaders/GLTFLoader.js';
 
 const video = document.getElementById('camera');
 const canvas = document.getElementById('qrCanvas');

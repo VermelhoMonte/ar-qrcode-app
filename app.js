a26a6cd6744ee43cceccba1344f46c69707b2412
Vermelho Monte <vermelho.monte@gmail.com>
2024-09-25 14:38:32 -0300

    250903

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 //import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 //import * as THREE from 'three';
-import * as THREE from './node_modules/three/src/Three.js';
-import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+//import * as THREE from './node_modules/three/src/Three.js';
+//import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
 const video = document.getElementById('camera');
 const canvas = document.getElementById('qrCanvas');
